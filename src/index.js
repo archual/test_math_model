434c7b426907217dea44602e279ab1501acd0140
archual <archual@mail.ru>
2022-07-20 19:38:16 +0300

    Add main logic for test system

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import "./styles.css";
+import { v4 as uuidv4 } from "uuid";
+
+class Client {
+  id;
+  wallet;
+  tickets = [];
+
+  constructor() {
+    this.id = uuidv4();
+    this.wallet = Math.floor(Math.random() * 250 + 30);
+  }
+
+  buy(tickets) {
+    this.tickets = [...tickets];
+  }
+
+  getPrize(winTicket, amount) {
+    this.ticket = this.ticket.filter((ticket) => ticket !== winTicket);
+    this.wallet += amount;
+  }
+}
 
 class Contract {
+  price = 10;
   GAS_FEE = 0.01;
   OWNER_FEE = 0.02;
   OS_FEE = 0.02;
@@ -9,32 +31,214 @@ class Contract {
   prizeJackPot = 0.1;
 
   tickets = [];
+  soldTickets = [];
+  conTimer;
+  conTimerCount = 0;
+  jpTimer;
+  jpTimerCount = 0;
 
-  constructor({ gasFee, ownerFee, osFee, prizeMainPart, prizeJackPot }) {
-    if (gasFee) {
-      this.GAS_FEE = gasFee;
-      this.OWNER_FEE = ownerFee;
-      this.OS_FEE = osFee;
+  feesWallets = {
+    os: 0,
+    owner: 0,
+    gas: 0
+  };
+  conWallet = 100;
+  jpWallet = 0;
 
-      this.prizeMainPart = prizeMainPart;
-      this.prizeJackPot = prizeJackPot;
+  iterations = 100;
+
+  constructor({ gasFee, ownerFee, osFee, prizeMainPart, prizeJackPot } = {}) {
+    // if (arguments) {
+    //   this.GAS_FEE = gasFee;
+    //   this.OWNER_FEE = ownerFee;
+    //   this.OS_FEE = osFee;
+    //   this.prizeMainPart = prizeMainPart;
+    //   this.prizeJackPot = prizeJackPot;
+    // }
+  }
+
+  generateTickets(amount) {
+    for (let i = 0; i < amount; i++) {
+      // generate 6 numbers from 1 to 49 unique
+      // write them in string with -
+      const numbers = [];
+      while (numbers.length !== 5) {
+        const number = Math.ceil(Math.random() * 49);
+        if (!numbers.includes(number)) numbers.push(number);
+      }
+      const ticketStr = numbers.join("-");
+      this.tickets = [...this.tickets, ticketStr];
     }
   }
 
-  run() {}
+  start() {
+    this.conTimer = this.startCon();
+    this.jpTimer = this.startJP();
+  }
 
-  generateTickets(amount) {
-    for (let i; i < amount; i++) {
-      this.tickets.push();
+  stop() {
+    clearInterval(this.conTimer);
+    clearInterval(this.jpTimer);
+  }
+
+  startCon() {
+    const interval = setInterval(() => {
+      // Prize logic
+      console.log("Contest");
+
+      // get all sold tickets
+      // Generate 6 unique numbers from 1 to 49
+      const winNumbers = [];
+      while (winNumbers.length !== 5) {
+        const number = Math.ceil(Math.random() * 49);
+        if (!winNumbers.includes(number)) winNumbers.push(number);
+      }
+      // Check every ticket, how much matches
+
+      // divide prizes before winners.
+      this.conTimer++;
+      if (this.conTimer > this.iterations) {
+        clearInterval(this.conTimer);
+        clearInterval(this.jpTimer);
+      }
+    }, 1000 * 60);
+    return interval;
+  }
+
+  startJP() {
+    const interval = setInterval(() => {
+      // Prize logic
+      console.log("Jack Pot");
+    }, 1000 * 60 * 5);
+    return interval;
+  }
+
+  sell(client, amount) {
+    const tickets = [];
+    for (let i = 0; i < amount; i++) {
+      tickets.push(this.tickets.pop());
     }
+    client.buy(tickets);
+    this.soldTickets = [...this.soldTickets, ...tickets];
+
+    // console.log({ fee: this.GAS_FEE });
+    const gas = amount * this.price * this.GAS_FEE;
+    const owner = amount * this.price * this.OWNER_FEE;
+    const os = amount * this.price * this.OS_FEE;
+    this.feesWallets.gas += gas;
+    this.feesWallets.owner += owner;
+    this.feesWallets.os += os;
+
+    this.conWallet += (amount * this.price - (gas + owner + os)) * 0.9;
+    this.jpWallet += (amount * this.price - (gas + owner + os)) * 0.1;
+  }
+
+  getTicketsAmount() {
+    return this.tickets.length;
   }
 }
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// System for emulating
+class TestSystem {
+  buyTimer;
+  buyTimerCount = 0;
+  Contract;
+  clients = [];
+  logTimer;
+  logTimerCount = 0;
+  iterations = 100;
+
+  constructor() {
+    this.Contract = new Contract();
+    for (let i = 0; i < 1000; i++) {
+      this.clients.push(new Client());
+    }
+  }
+
+  run() {
+    this.Contract.generateTickets(10000);
+    // console.log(this.Contract.tickets);
+    console.log("ticketa:", this.Contract.getTicketsAmount());
+  }
+
+  start() {
+    if (!this.Contract) return;
+    console.log("start");
+    this.Contract.start();
+    this.buyTimer = this.startBuyTimer();
+    this.logTimer = this.startlog();
+  }
+
+  stop() {
+    if (!this.Contract) return;
+    console.log("stop");
+    this.Contract.stop();
+    clearInterval(this.buyTimer);
+    clearInterval(this.logTimer);
+  }
+
+  startBuyTimer() {
+    const interval = setInterval(() => {
+      // Buy logic
+      // check amount of rickets
+      const ticketsLeft = this.Contract.getTicketsAmount();
+      // choose amount of tickets
+      const amount = Math.floor(
+        Math.random() * (ticketsLeft > 5 ? 5 : ticketsLeft) + 1
+      );
+      // choose client
+      const currentClient = this.clients[
+        Math.floor(Math.random() * this.clients.length)
+      ];
+
+      // check clietn wallet
+      if (currentClient.wallet > amount * this.Contract.price) {
+        // buy tickets.
+        this.Contract.sell(currentClient, amount);
+      }
+      this.buyTimerCount++;
+      if (this.buyTimerCount > this.iterations * 10) {
+        clearInterval(this.buyTimer);
+      }
+    }, 100);
+    return interval;
+  }
+
+  startlog() {
+    this.logTimer = setInterval(() => {
+      this.logResults();
+      this.logTimerCount++;
+      if (this.logTimerCount > this.iterations) {
+        clearInterval(this.logTimer);
+      }
+    }, 1000);
+    console.log(this.Contract.wallet);
+    console.log(this.clients);
+  }
+
+  logResults() {
+    document.getElementById("contractWallet").innerHTML = JSON.stringify({
+      conWallet: Math.ceil(this.Contract.conWallet),
+      jpWallet: Math.ceil(this.Contract.jpWallet),
+      owner: Math.ceil(this.Contract.feesWallets.owner)
+    });
+    document.getElementById(
+      "contractTikets"
+    ).innerHTML = this.Contract.tickets.length;
+    // const clientsStr = this.clients.map(
+    //   (client) => `${client.id}: ${client.wallet} /n`
+    // );
+    // document.getElementById("clients").innerHTML = this.Contract.tickets
+    //   .map((ticket) => `<p>${ticket}</p`)
+    //   .join("");
+  }
+}
+
+const test = new TestSystem();
+
+var start = document.getElementById("start");
+start.addEventListener("click", test.start.bind(test), false);
+var stop = document.getElementById("stop");
+stop.addEventListener("click", test.stop.bind(test), false);
+
+test.run();
